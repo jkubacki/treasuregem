6e07bcba827a6ef9331dc69a28ad5d538f17460d
Jakub Kubacki <kubacki.jk@gmail.com>
2016-08-25 19:20:26 +0200

    Dynamic template

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  name = 'Gold Nugget'
+  value = 1000.00
+  context = { 'treasure_name': name,
+              'treasure_val': value }
+  return render(request, 'index.html', context)
