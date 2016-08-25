7549261fc673610204b08519d6afa5b63334e4bd
Jakub Kubacki <kubacki.jk@gmail.com>
2016-08-25 20:04:52 +0200

    Use model in view

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
+from .models import Treasure
 
 # Create your views here.
 def index(request):
-  name = 'Gold Nugget'
-  value = 1000.00
-  context = { 'treasures': treasures }
-  return render(request, 'index.html', context)
-
-
-class Treasure:
-  def __init__(self, name, value, material, location):
-    self.name = name
-    self.value = value
-    self.material = material
-    self.location = location
-
-treasures = [
-  Treasure('Gold Nugget', 5000, 'gold', 'somewhere'),
-  Treasure('Silver Nugget', 2000, 'silver', 'poznan')
-]
+  treasures = Treasure.objects.all()
+  return render(request, 'index.html', { 'treasures': treasures })
